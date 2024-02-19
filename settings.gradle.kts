5cebe07bd42ce179906bcf877e34d68c0d03dd05
alsdl0629 <ehfehf0629@gmail.com>
2024-02-19 19:02:46 +0900

    Chore: ktlint 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
     val kotlinVersion: String by settings
     val springBootVersion: String by settings
     val springDependencyManagementVersion: String by settings
+    val ktlintVersion: String by settings
 
     resolutionStrategy {
         eachPlugin {
@@ -14,6 +15,7 @@ pluginManagement {
                 "org.jetbrains.kotlin.plugin.jpa" -> useVersion(kotlinVersion)
                 "org.springframework.boot" -> useVersion(springBootVersion)
                 "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+                "org.jlleitschuh.gradle.ktlint" -> useVersion(ktlintVersion)
             }
         }
     }
